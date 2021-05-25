a375b3f7f8999d3b3727fbc6e9522e2b673e3197
Julien Bosse <julien@bosse.pro>
2021-05-25 14:39:30 +0200

    Correction et UML

--- JeuDeCreatures.py
+++ JeuDeCreatures.py
@@ -2,11 +2,11 @@ from random import choice
 
 class Case:
 
-    def __init__(self,x,y):
-        self.x=x
-        self.y=y
+    def __init__(self,x: int,y: int):
+        self.x = x
+        self.y = y
 
-    def adjacentes(self,jeu):
+    def adjacentes(self,jeu) -> list:
         # Retourne les cases de la liste des cases dont la distance**2 est inférieure ou égale à 2 à la case courante
         return [c for c in jeu.listeDesCases if 0 < (c.x-self.x)**2+(c.y-self.y)**2 <= 2]
         
@@ -15,14 +15,34 @@ class Case:
         s = "(x: "+str(self.x)+", y: "+str(self.y)+")"
         return s
         
+class Creature:
 
+    def __init__(self, nom: str, position: Case):
+        self.nom=nom
+        self.position=position
+
+    def choisirCible(self, jeu):
+        print("___ CHOIX DE LA CIBLE ___")
+        listeAdjacentes = self.position.adjacentes(jeu)
+        for c in listeAdjacentes:
+            if jeu.estOccupee(c) :
+                # print("Cible occupée ", c)
+                return c
+
+        cible = choice(listeAdjacentes)
+        # print("Cible aléatoire ", cible)
+        return cible
+
+    def __str__(self):
+        return str(self.nom)+" | "+str(self.position)
 
 class Jeu:
-    def __init__(self,listeDesCases,listeDesCreatures,tour,actif):
+    def __init__(self,listeDesCases: list,listeDesCreatures: list):
         self.listeDesCases=listeDesCases
         self.listeDesCreatures=listeDesCreatures
-        self.tour=tour
-        self.actif=actif
+        self.tour=1
+        self.actif=listeDesCreatures[0]
+        
         print("########## DÉBUT DU JEU ##########")
 
     def estOccupee(self,case):
@@ -34,7 +54,7 @@ class Jeu:
 
 
     def deplacer(self,creature,case):
-        print("_____ DEPLACEMENT ______")
+        print("______ DEPLACEMENT ______")
         print("Tour :", self.tour)
         print("C'est à", self.actif, "de jouer")
         if self.actif == creature :
@@ -42,24 +62,18 @@ class Jeu:
 
             for c in listeAdjacentes :
                 if c.x == case.x and c.y == case.y:
-                    print("La case est bien adjacente")
-
-                    print("La case est occupée ?",self.estOccupee(case))
                     if self.estOccupee(case):
                         creature.position.x = case.x
                         creature.position.y = case.y
                         print()
-                        print()
-                        self.plateau()
-                        print()
                         print("################################")
-                        print("  ### Vainqueur : "+ creature.nom + " ###")
+                        print("  ### Vainqueur :  "+ creature.nom + "  ###")
                         print("################################")
                         self.tour = 0
                     else:
                         creature.position.x = case.x
                         creature.position.y = case.y
-                        self.actif = listeDesCreatures[self.tour%2]
+                        self.actif = listeDesCreatures[self.tour%len(listeDesCreatures)]
                         self.tour += 1
                         print("\n")
                     break
@@ -75,10 +89,9 @@ class Jeu:
             print("|",end="")
             for y in range(int(len(self.listeDesCases)**(1/2))):
                 if self.estOccupee(self.listeDesCases[i]):
-                    if self.listeDesCases[i].x == listeDesCreatures[0].position.x and self.listeDesCases[i].y == listeDesCreatures[0].position.y:
-                        print(" "+listeDesCreatures[0].nom[0]+" |",end="")
-                    else:
-                        print(" "+listeDesCreatures[1].nom[0]+" |",end="")
+                    for c in listeDesCreatures:
+                        if c.position.x == listeDesCases[i].x and c.position.y == listeDesCases[i].y:
+                            print(" "+c.nom[0]+" |",end="")
                 else:
                     print(" _ |",end="")
                 i+=1
@@ -89,48 +102,26 @@ class Jeu:
         s = str(self.listeDesCases)+"\n"+str(self.listeDesCreatures)+"\n"+str(self.tour)+str(self.actif)
         return s
 
-class Creature:
-
-    def __init__(self,nom,position):
-        self.nom=nom
-        self.position=position
-
-    def choisirCible(self,jeu):
-        print("___ CHOIX DE LA CIBLE ___")
-        listeAdjacentes = self.position.adjacentes(jeu)
-        # for i in listeAdjacentes: print(i)
-        for c in listeAdjacentes:
-            if jeu.estOccupee(c) :
-                print("Cible occupée ", c)
-                print(c)
-                return c
-
-        cible = choice(listeAdjacentes)
-        print("Cible aléatoire ", cible)
-        return cible
-
-    def __str__(self):
-        return str(self.nom)+" | "+str(self.position)
-
 from time import sleep
 
 # Création de la partie
-xmax=4
-ymax=4
+xmax=8
+ymax=8
 
 crea1 = Creature("Garurumon",Case(xmax,ymax))
 crea2 = Creature("Dreymon",Case(0,0))
-listeDesCreatures = [crea1, crea2]
+crea3 = Creature("MetalGreymon", Case(0,ymax))
+listeDesCreatures = [crea1, crea2, crea3]
+
 listeDesCases = [Case(x,y) for x in range(xmax) for y in range(ymax)]
 
-jeu = Jeu(listeDesCases, listeDesCreatures, 1, crea1)
+jeu = Jeu(listeDesCases, listeDesCreatures)
 
-# Boucle de la partie : affiche le plateau, choisi la cible pour la créature active et déplace cette-dernière
+# Boucle partie : affiche le plateau, choisit la cible pour la créature active et déplace cette dernière jusqu'à ce qu'il y ait un vainqueur
 i=0
 while jeu.tour != 0:
     sleep(0.5)
     jeu.plateau()
-    cible = listeDesCreatures[i%2].choisirCible(jeu)
-    jeu.deplacer(listeDesCreatures[i%2],cible)
+    cible = listeDesCreatures[i%len(listeDesCreatures)].choisirCible(jeu)
+    jeu.deplacer(listeDesCreatures[i%len(listeDesCreatures)],cible)
     i+=1
-
